04042fba5919d8592c4be140f5013c772dacfce7
Nic <nic@askable.com>
2021-06-08 16:36:49 +1000

    routes & icons

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import _ from 'lodash';
-import { Router, Route, Switch } from "react-router";
+import { Router, Route, Switch, Redirect } from "react-router";
 import { createBrowserHistory } from "history";
 
 // import Tech from './components/tech';
 import DiceTable from './components/dice-table';
-import FactionDeal from './components/faction-shuffle';
+import MapBuilder from './components/map-builder';
 
 import './styles/app.css';
 
@@ -15,8 +15,9 @@ export default () => {
   return (
   <Router history={history}>
       <Switch>
-        <Route path="/TI4/factions/:keys?" component={FactionDeal} />
-        <Route path="/" component={DiceTable} />
+        <Route path="/TI4/dice" component={DiceTable} />
+        <Route path="/TI4/map-builder" component={MapBuilder} />
+        <Route path="/" component={() => <Redirect to="/TI4/dice" />} />
       </Switch>
   </Router>
   );
