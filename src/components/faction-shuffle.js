f235d09ac4c608ae827e121eb03d63117022f572
Nic <nic@askable.com>
2021-06-08 14:31:01 +1000

    migrate yaml data to js

--- src/components/faction-shuffle.js
+++ src/components/faction-shuffle.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 const LIST_KEY_BASE = 36;
 const LIST_KEY_MAX_LENGTH = 1; // max 36 factions
-const FACTIONS = require("../data/faction-shuffle.yaml");
+const { FACTIONS } = require('../lib/constants');
 
 const CollapsibleSection = ({ title, children, className, ...props }) => {
     const [open, setOpen] = useState(!!props.open);
@@ -30,27 +30,51 @@ const CollapsibleSection = ({ title, children, className, ...props }) => {
 const FactionDeal = () => {
     const [playerCount, setPlayerCount] = useState(4);
     const [playerNames, setPlayerNames] = useState([]);
+    const [factionMax, setFactionMax] = useState(null);
 
-    const [usePOK, setUsePOK] = useState(true);
+    const [expansions, setExpansions] = useState({ pok: true });
 
-    function encodeList(keys) {
-        return window.btoa(keys.map(int => int.toString(LIST_KEY_BASE).padStart(LIST_KEY_MAX_LENGTH, '0')).join('')).replace(/=+$/, '');
+    const [factionList, setFactionList] = useState([]);
+    useEffect(() => {
+        setFactionList(FACTIONS.map(f => ({ ...f, selected: true })));
+    }, [])
+
+    // function encodeList(keys) {
+    //     return window.btoa(keys.map(int => int.toString(LIST_KEY_BASE).padStart(LIST_KEY_MAX_LENGTH, '0')).join('')).replace(/=+$/, '');
+    // }
+
+    function setExpansionValue(key, value) {
+        const newValue = _.clone(expansions);
+        newValue[key] = value;
+        setExpansions(newValue);
+
+        return newValue;
     }
+
+    function selectFaction(name, selected = true) {
+        const newValue = _.clone(factionList);
+        _.find(factionList, { name }).selected = selected;
+
+        setFactionList(newValue);
+        return newValue;
+    }
+
+    const defaultFactionsPerPlayer = 0;
 
     return (
         <div className="container dice-table my-2">
             <h2>Faction shuffler</h2>
-
+            
             <CollapsibleSection title="Players" className="mb-1" open >
                 <div className="form-group mb-1">
-                    <label htmlFor="input-player-count" className="mr-1">Count</label>
+                    <label htmlFor="input-player-count" className="mr-1">Player count</label>
                     <input
                         type="number"
                         // inputMode="numeric"
                         id="input-player-count"
                         className="form-control"
                         min={3}
-                        max={usePOK ? 8 : 6}
+                        max={expansions.pok ? 8 : 6}
                         value={playerCount ? playerCount.toString(10) : ''}
                         onChange={(e) => {
                             const value = parseInt(e.target.value, 10);
@@ -70,20 +94,73 @@ const FactionDeal = () => {
                             onChange={(e) => {
                                 const names = [...playerNames];
                                 names[i] = e.target.value.trim(/^\s+|\s+$/g, '');
-                                console.log(names);
                                 setPlayerNames(names);
                             }}
                         />
                     ))}
                 </div>
+                <div className="form-group mb-1">
+                    <label htmlFor="input-faction-count" className="mr-1">Max factions per player</label>
+                    <input
+                        type="number"
+                        // inputMode="numeric"
+                        id="input-faction-count"
+                        className="form-control"
+                        placeholder={defaultFactionsPerPlayer || '-'}
+                        min={1}
+                        value={factionMax ? factionMax.toString(10) : ''}
+                        onChange={(e) => {
+                            const { value } = e.target;
+                            setFactionMax(value ? parseInt(value, 10) : null);
+                        }}
+                    />
+                </div>
             </CollapsibleSection>
 
-            <CollapsibleSection title="Exclude" className="mb-1">
-                BIG LIST
+            <CollapsibleSection title="Factions" className="mb-1">
+                {factionList.map((faction, i) => (
+                    <div className="form-group form-check" key={faction.name}>
+                        <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id={`faction-checkbox-${faction.key}`}
+                            name="factions"
+                            checked={!!(faction.selected && (expansions.pok || !faction.pok))}
+                            disabled={faction.pok && !expansions.pok}
+                            onChange={(e) => {
+                                selectFaction(faction.name, !!e.target.checked)
+                            }}
+                            value={faction.key}
+                        />
+                        <label className="form-check-label" htmlFor={`faction-checkbox-${i}`}>{faction.name}</label>
+                    </div>
+                ))}
+
+                <button
+                    type="button"
+                    className="btn btn-sm btn-secondary"
+                    onClick={() => {
+                        const selected = _.sumBy(factionList, 'selected') < factionList.length;
+                        setFactionList(factionList.map(f => ({ ...f, selected })));
+                    }}
+                >
+                    Select {_.sumBy(factionList, 'selected') < factionList.length ? 'all' : 'none'}
+                </button>
             </CollapsibleSection>
 
             <CollapsibleSection title="Expansion options" className="mb-1">
-                BIG LIST
+                <div className="form-group form-check">
+                    <input
+                        type="checkbox"
+                        className="form-check-input"
+                        id="expansion-checkbox-pok"
+                        checked={!!expansions.pok}
+                        onChange={(e) => {
+                            setExpansionValue('pok', !!e.target.checked);
+                        }}
+                    />
+                    <label className="form-check-label" htmlFor="expansion-checkbox-pok">Prophecy of Kings</label>
+                </div>
             </CollapsibleSection>
         </div>
     );
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -17,6 +17,115 @@ const GAME_COLOURS = {
     PURPLE_2: '#6500B4',
 };
 
+const FACTIONS = [
+    // BASE GAME
+    {
+        key: 'AR',
+        name: 'The Arborec',
+    },
+    {
+        key: 'BL',
+        name: 'The Barony of Letnev',
+    },
+    {
+        key: 'CS',
+        name: 'The Clan of Saar',
+    },
+    {
+        key: 'EM',
+        name: 'The Embers of Muaat',
+    },
+    {
+        key: 'EH',
+        name: 'The Emirates of Hacan',
+    },
+    {
+        key: 'EP',
+        name: 'The Empyrean',
+    },
+    {
+        key: 'FS',
+        name: 'The Federation of Sol',
+    },
+    {
+        key: 'GC',
+        name: 'The Ghosts of Creuss',
+    },
+    {
+        key: 'LM',
+        name: 'The L1Z1X Mindnet',
+    },
+    {
+        key: 'MC',
+        name: 'The Mentak Coalition',
+    },
+    {
+        key: 'NC',
+        name: 'The Naalu Collective',
+    },
+    {
+        key: 'NV',
+        name: 'The Nekro Virus',
+    },
+    {
+        key: 'SN',
+        name: 'Sardakk N’orr',
+    },
+    {
+        key: 'UJ',
+        name: 'The Universities of Jol-Nar',
+    },
+    {
+        key: 'WN',
+        name: 'The Winnu',
+    },
+    {
+        key: 'XK',
+        name: 'The Xxcha Kingdom',
+    },
+    {
+        key: 'YB',
+        name: 'The Yin Brotherhood',
+    },
+    {
+        key: 'YT',
+        name: 'The Yssaril Tribes',
+    },
+
+    // POK
+    {
+        key: 'AF',
+        name: 'The Argent Flight',
+        pok: TRUE
+    },
+    {
+        key: 'MG',
+        name: 'The Mahact Gene-Sorcerers',
+        pok: TRUE
+    },
+    {
+        key: 'NR',
+        name: 'The Naaz-Rokha Alliance',
+        pok: TRUE
+    },
+    {
+        key: 'NM',
+        name: 'The Nomad',
+        pok: TRUE
+    },
+    {
+        key: 'TU',
+        name: 'The Titans of Ul',
+        pok: TRUE
+    },
+    {
+        key: 'VR',
+        name: 'The Vuil’Raith Cabal',
+        pok: TRUE
+    },
+]
+
 export {
-    GAME_COLOURS
+    GAME_COLOURS,
+    FACTIONS,
 }
