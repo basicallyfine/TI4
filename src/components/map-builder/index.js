34eaffac1c8adbeab5e9c08549183bd60d73c69d
Nic <nic@askable.com>
2021-06-09 10:25:44 +1000

    install dnd; update react dependencies

--- src/components/map-builder/index.js
+++ src/components/map-builder/index.js
@@ -1,1 +1,14 @@
-export default () => null;
+import React from 'react';
+
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+const MapBuilder = () => (
+    <div id="map-builder">
+        <DndProvider backend={HTML5Backend}>
+            <div />
+        </DndProvider>
+    </div>
+);
+
+export default MapBuilder;
