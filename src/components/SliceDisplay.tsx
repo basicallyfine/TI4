f2c2ef49f157508913da50c375d80594ad0608ae
Nic <nic@askable.com>
2024-10-21 10:16:26 +1000

    Add slice summary table

--- src/components/SliceDisplay.tsx
+++ src/components/SliceDisplay.tsx
@@ -6,6 +6,42 @@ import { SYSTEM_PNG_URL } from "./map-builder/map-constants";
 
 import "../styles/slice-display.scss";
 
+const SLICE_LABELS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+];
+
+type SliceValues = {
+  resources_max: number;
+  influence_max: number;
+  resources_efficient: number;
+  influence_efficient: number;
+  planets: number;
+};
+
+type SliceDetails = {
+  key: string;
+  label: string;
+  systems: ((typeof SYSTEMS)[number] | null)[];
+  value: SliceValues;
+  non_equi_value: SliceValues;
+  attributes: string[];
+};
+
+function valueLabel(value: number) {
+  return value.toString(10).replace(".5", "½");
+}
+
 export const SliceDisplay = () => {
   const [sliceString, setSliceString] = useState<string | undefined>();
   const [sorting, setSorting] = useState<string | null>();
@@ -24,7 +60,12 @@ export const SliceDisplay = () => {
           paramValues.slice.replace(/^[\s,;|]+|[\s,;|]+$/g, "")
         )
       );
-    } else if (params.keys()?.next().value?.match(/^[\d,;\s]+$/)) {
+    } else if (
+      params
+        .keys()
+        ?.next()
+        .value?.match(/^[\d,;\s]+$/)
+    ) {
       const newUrl = new URL(window.location.href);
       newUrl.search = `s=${newUrl.search}`;
       window.location.replace(newUrl.toString());
@@ -36,36 +77,90 @@ export const SliceDisplay = () => {
 
   const slices = useMemo(() => {
     if (sliceString) {
-      const _s = sliceString.split(/\s*\|\s*|\s*;\s*/).map((s) => {
-        const slice = {
+      const _s = sliceString.split(/\s*\|\s*|\s*;\s*/).map((s, sliceIdx) => {
+        const slice: SliceDetails = {
           key: s,
+          label: SLICE_LABELS[sliceIdx],
           systems: s
             .split(/\s+|\s*,\s*/)
-            .map((n) =>
-              n && n.match(/^\d+$/)
-                ? SYSTEMS.find(({ number }) => number === Number(n))
-                : null
+            .map(
+              (n) =>
+                (n && n.match(/^\d+$/)
+                  ? SYSTEMS.find(({ number }) => number === Number(n))
+                  : null) ?? null
             ),
           value: {
             resources_max: 0,
             influence_max: 0,
             resources_efficient: 0,
             influence_efficient: 0,
+            planets: 0,
           },
+          non_equi_value: {
+            resources_max: 0,
+            influence_max: 0,
+            resources_efficient: 0,
+            influence_efficient: 0,
+            planets: 0,
+          },
+          attributes: [],
         };
-        slice.systems.forEach((system) => {
+        slice.systems.forEach((system, systemIdx) => {
+          const isEquidistant = systemIdx === 3;
+          slice.value.planets += system?.planets.length || 0;
+          if (!isEquidistant) {
+            slice.non_equi_value.planets += system?.planets.length || 0;
+          }
           system?.planets.forEach((planet) => {
-            slice.value.resources_max += planet.resources;
-            slice.value.influence_max += planet.influence;
+            const systemValue = {
+              resources_max: 0,
+              influence_max: 0,
+              resources_efficient: 0,
+              influence_efficient: 0,
+            };
+
+            if (planet.tech) {
+              slice.attributes.push(
+                `${planet.tech}${isEquidistant ? " (E)" : ""}`
+              );
+            }
+            if (planet.legendary) {
+              slice.attributes.push(
+                `${planet.name}${isEquidistant ? " (E)" : ""}`
+              );
+            }
+
+            systemValue.resources_max += planet.resources;
+            systemValue.influence_max += planet.influence;
             if (planet.resources > planet.influence) {
-              slice.value.resources_efficient += planet.resources;
+              systemValue.resources_efficient += planet.resources;
             } else if (planet.influence > planet.resources) {
-              slice.value.influence_efficient += planet.influence;
+              systemValue.influence_efficient += planet.influence;
             } else {
-              slice.value.resources_efficient += planet.resources / 2;
-              slice.value.influence_efficient += planet.influence / 2;
+              systemValue.resources_efficient += planet.resources / 2;
+              systemValue.influence_efficient += planet.influence / 2;
+            }
+
+            slice.value.resources_max += systemValue.resources_max;
+            slice.value.influence_max += systemValue.influence_max;
+            slice.value.resources_efficient += systemValue.resources_efficient;
+            slice.value.influence_efficient += systemValue.influence_efficient;
+
+            if (!isEquidistant) {
+              slice.non_equi_value.resources_max += systemValue.resources_max;
+              slice.non_equi_value.influence_max += systemValue.influence_max;
+              slice.non_equi_value.resources_efficient +=
+                systemValue.resources_efficient;
+              slice.non_equi_value.influence_efficient +=
+                systemValue.influence_efficient;
             }
           });
+
+          if (system?.wormhole) {
+            slice.attributes.push(
+              `${system.wormhole}${isEquidistant ? " (E)" : ""}`
+            );
+          }
         });
         return slice;
       });
@@ -91,39 +186,91 @@ export const SliceDisplay = () => {
   }, [sliceString, sorting]);
 
   return (
-    <div className="container-fluid" id="slice-display">
-      {slices &&
-        slices.map(({ key, systems, value }) => (
-          <div key={key} className="slice">
-            <div className="tiles">
-              {systems.map(
-                (system, i) =>
-                  system && (
-                    <div
-                      key={system.number}
-                      className={`tile tile-${i}`}
-                      style={{
-                        backgroundImage: `url(${SYSTEM_PNG_URL}${system.number}.png)`,
-                      }}
-                    >
-                      <span className="label">{system.number}</span>
-                    </div>
-                  )
-              )}
-            </div>
-            <div className="info">
-              <div className="wrapper">
-                <span>
-                  {value.resources_max}/{value.influence_max}
-                </span>
-                <span>
-                  ({value.resources_efficient.toString(10).replace(".5", "½")}/
-                  {value.influence_efficient.toString(10).replace(".5", "½")})
-                </span>
+    <>
+      <div className="container-fluid" id="slice-display">
+        {slices &&
+          slices.map(({ key, systems, value }) => (
+            <div key={key} className="slice">
+              <div className="tiles">
+                {systems.map(
+                  (system, i) =>
+                    system && (
+                      <div
+                        key={system.number}
+                        className={`tile tile-${i}`}
+                        style={{
+                          backgroundImage: `url(${SYSTEM_PNG_URL}${system.number}.png)`,
+                        }}
+                      >
+                        <span className="label">{system.number}</span>
+                      </div>
+                    )
+                )}
+              </div>
+              <div className="info">
+                <div className="wrapper">
+                  <span>
+                    {value.resources_max}/{value.influence_max}
+                  </span>
+                  <span>
+                    ({valueLabel(value.resources_efficient)}/
+                    {valueLabel(value.influence_efficient)})
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-    </div>
+          ))}
+      </div>
+      <div className="container">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>-</th>
+              <th>Optimal</th>
+              <th>Max</th>
+              <th>Optimal+E</th>
+              <th>Max+E</th>
+              <th>Planets</th>
+              <th>Planets+E</th>
+              <th>Skips</th>
+            </tr>
+          </thead>
+          <tbody>
+            {slices &&
+              slices.map(
+                ({
+                  key,
+                  label,
+                  value,
+                  non_equi_value,
+                  attributes,
+                }) => (
+                  <tr key={key}>
+                    <td>{label}</td>
+                    <td>
+                      {valueLabel(non_equi_value.resources_efficient)} /{" "}
+                      {valueLabel(non_equi_value.influence_efficient)}
+                    </td>
+                    <td>
+                      {non_equi_value.resources_max} /{" "}
+                      {non_equi_value.influence_max}
+                    </td>
+                    <td>
+                      {valueLabel(value.resources_efficient)} /{" "}
+                      {valueLabel(value.influence_efficient)}
+                    </td>
+                    <td>
+                      {value.resources_max} / {value.influence_max}
+                    </td>
+                    <td>{non_equi_value.planets}</td>
+                    <td>{value.planets}</td>
+                    <td>{attributes.join(", ")}</td>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
